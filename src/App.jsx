9ff2778f01c7bee4a57bed05a5bebefde2c587cf
M. Nadhir Fauzananto <nadhir.fauzananto@gmail.com>
2026-05-18 08:28:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {ContactForm} from './ContactForm.jsx'
+
+export function App() {
+    return (
+        <div className="app-container">
+            <ContactForm />
+        </div>
+    )
+}
---
+++ src/ContactForm.jsx
@@ -0,0 +1,242 @@
+import React from 'react'
+
+export function ContactForm() {
+
+    const [formData, setFormData] = React.useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: ''
+    })
+    
+    const [queryType, setQueryType] = React.useState(null)
+    const [consent, setConsent] = React.useState(false)
+
+    const [error, setError] = React.useState({})
+    const [submitStatus, setSubmitStatus] = React.useState('')
+
+    const validateForm = () => {
+        const errors = {}
+
+        if (formData.firstName.trim().length < 1) {
+            errors.firstName = 'First name is required.'
+            return errors
+        }   else {
+            return errors.firstName = ''
+        }
+
+        if (formData.lastName.trim().length < 1) {
+            errors.lastName = 'Last name is required.'
+            return errors
+        }   else { 
+            return errors.lastName = ''
+        }
+
+        if (formData.email.trim().length < 1) {
+            errors.email = 'A valid email address is required.'
+            return errors
+
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(formData.email)) {
+            errors.email = 'Invalid email address.'
+            return errors
+        } else {
+            return errors.email = ''
+        }
+
+        if (formData.message.trim().length < 1) {
+            errors.message = 'Message cannot be empty.'
+            return errors
+        }   else {
+            return errors.message = ''
+        }
+
+        if (!queryType) {
+            errors.queryType = 'Please select a query type.'
+            return errors
+        } else {
+            return errors.queryType = ''
+        }
+
+        if (!consent) {
+            errors.consent = 'You must agree to be contacted.'
+            return errors
+        }   return errors.consent = ''
+    }
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+
+        if (error[name]) {
+            setError(prevErrors => ({
+                ...prevErrors,
+                [name]: ''
+            }))
+        }
+    }
+
+    const [isFormValid, setIsFormValid] = React.useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const errors = validateForm()
+        console.log(errors)
+        if (Object.keys(errors).length > 0) {
+            setError(errors)
+            setIsFormValid(false)
+            setSubmitStatus('error')
+            return
+        }
+
+        const data = {
+                firstName: formData.firstName,
+                lastName: formData.lastName,
+                email: formData.email,
+                message: formData.message,
+                queryType: queryType,
+                consent: consent
+            }
+
+        setIsFormValid(true)
+        setSubmitStatus('success')
+        console.log(data)
+    }
+    
+    return (
+        <div className="contact-form-container">
+            <div className="contact-message-container">
+                <h1>Contact Us</h1>
+                {submitStatus === 'success' ? 
+                <div className="submit-status-success-container">
+                    <div className="submit-status-success-checkmark">
+                        <img src="./images/icon-success-check.svg" alt="Success checkmark"></img>
+                        <h2>Message Sent!</h2>
+                    </div>
+                    <p className="submit-status-success-message">Your data has been successfully submitted! Our team will contact you as soon as possible.</p>
+                </div> : 
+                null}
+            </div>
+
+            <form className="contact-form">
+                <div className="name-container">
+                    <div className="first-name-container">
+                        <label htmlFor="first-name">First Name *</label>
+                        <input 
+                        type="text" 
+                        placeholder="John" 
+                        id="first-name" 
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleInputChange}
+                        aria-required="true"
+                        aria-label="Please enter your first name."
+                        />
+                        {formData.firstName === "" ? 
+                        <p className="submit-status-error">This field is required.</p> : null}
+                    </div>
+
+                    <div className="last-name-container">
+                        <label htmlFor="last-name">Last Name *</label>
+                        <input 
+                        type="text" 
+                        placeholder="Doe" 
+                        id="last-name" 
+                        name="lastName" 
+                        value={formData.lastName} 
+                        onChange={handleInputChange}
+                        aria-required="true"
+                        aria-label="Please enter your last name."
+                        />
+                        {formData.lastName === "" ? 
+                        <p className="submit-status-error">This field is required.</p> : null}
+                    </div>
+                </div>
+
+                <div className="email-container">
+                    <label htmlFor="email">Email *</label>
+                    <input 
+                    type="email" 
+                    placeholder="john.doe@example.com" 
+                    id="email" 
+                    name="email" 
+                    value={formData.email} 
+                    onChange={handleInputChange}
+                    aria-required="true"
+                    aria-label="Please enter your email address."
+                    />
+                    {formData.email === "" || !/\S+@\S+\.\S+/.test(formData.email) ? 
+                    <p className="submit-status-error">Please enter a valid email address</p> : null}
+                </div>
+
+                <div className="query-container">
+                    <p>Query Type *</p>
+                    <div className="query-type-container">
+                        <div  className="query-type">
+                            <input         
+                            type="radio" 
+                            name="query" 
+                            id="general-query" 
+                            checked={queryType === 'general-query'} 
+                            onChange={() => setQueryType('general-query')}
+                            aria-required="true"
+                            aria-label="Check this for general query."/>
+                            <label htmlFor="general-query">General Query</label>
+                        </div>
+
+                        <div className="query-type">                       
+                            <input 
+                            type="radio" 
+                            name="query" 
+                            id="support-request" 
+                            checked={queryType === 'support-request'} 
+                            onChange={() => setQueryType('support-request')}
+                            aria-required="true"
+                            aria-label="Check this for support request."/>
+                            <label htmlFor="support-request">Support Request</label>
+                        </div>
+                    </div>
+                    {queryType === null ? 
+                    <p className="submit-status-error">Please select a query type.</p> : null}
+                </div>
+
+                <div className="message-container">
+                    <label htmlFor="message">Message *</label>
+                    <textarea 
+                    placeholder="Your message" 
+                    id="message" 
+                    name="message" 
+                    value={formData.message} 
+                    onChange={handleInputChange}
+                    aria-required="true"
+                    aria-label="Please enter your message."></textarea>
+                    {formData.message === "" ? 
+                    <p className="submit-status-error">Please enter a message.</p> : null}
+                </div>
+
+                <div className="consent-container">
+                    <span className="checkmark-checkbox"></span> 
+                    <input 
+                    type="checkbox" 
+                    id="consent" 
+                    name="consent" 
+                    checked={consent} 
+                    onChange={() => setConsent(!consent)}
+                    aria-required="true"
+                    aria-label="Check this to agree to being contacted by the team."/>
+                    <label htmlFor="consent">I agree to being contacted by the team. *</label>
+                    {!consent ? 
+                    <p className="submit-status-error">To submit this form, you must agree to being contacted by our team.</p> : null}
+                </div>
+
+                <button 
+                type="submit" 
+                className="submit-btn" 
+                onClick={handleSubmit}
+                aria-label="A button to submit the form.">Submit</button>
+            </form>
+        </div>
+    )
+}
